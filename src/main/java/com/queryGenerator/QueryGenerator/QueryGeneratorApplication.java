391f96f723a754b4a53f2fdf2b2a290bb4dd09b3
andre <andre@FENRIR>
2021-07-11 12:24:14 -0600

    Inicializa repositorio

---
+++ src/main/java/com/queryGenerator/QueryGenerator/QueryGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.queryGenerator.QueryGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryGeneratorApplication.class, args);
+	}
+
+}
